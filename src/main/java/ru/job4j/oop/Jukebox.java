918d40f67f164bf460a3d025b2a455ee0c8c243c
is.ivanov <is.ivanov@action-media.ru>
2022-08-09 08:39:27 +0300

    1.3. Вызов метода с аргументами. [#166086 #329873]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -2,6 +2,7 @@ package ru.job4j.oop;
 
 public class Jukebox {
     public static void main(String[] args) {
+
         Jukebox jukebox = new Jukebox();
         jukebox.music(1);
         jukebox.music(2);
