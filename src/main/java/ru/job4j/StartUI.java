61a8652c3fc99d4e8d128feea0861cf8e357b2f0
is.ivanov <is.ivanov@action-media.ru>
2022-08-16 15:52:29 +0300

    7. Date. Отображение даты. [#310017 #332637]

--- src/main/java/ru/job4j/StartUI.java
+++ src/main/java/ru/job4j/StartUI.java
@@ -3,11 +3,10 @@ package ru.job4j;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
-    Item item = new Item();
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-    String dataTime = item.getCreated().format(formatter);
-
-    public void getLocalTime() {
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String dataTime = item.getCreated().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + dataTime);
     }
 }
