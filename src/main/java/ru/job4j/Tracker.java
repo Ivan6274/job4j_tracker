045cf870d97d827b3ca43fec83f63805d3e3a9be
is.ivanov <is.ivanov@action-media.ru>
2022-08-17 09:20:35 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/Tracker.java
+++ src/main/java/ru/job4j/Tracker.java
@@ -13,17 +13,17 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
-    }
+//    public Item findById(int id) {
+//        Item rsl = null;
+//        for (int index = 0; index < size; index++) {
+//            Item item = items[index];
+//            if (item.getId() == id) {
+//                rsl = item;
+//                break;
+//            }
+//        }
+//        return rsl;
+//    }
 
     public Item[] findAll() {
         int count = 0;
@@ -47,5 +47,29 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(rsl, count);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean rsl = false;
+        if (findById(id) != null) {
+            findById(id).setName(item.getName());
+        }
+        return rsl;
     }
 }
