ebc5db332d3dde16d90ea3bc06c8deec157d575a
is.ivanov <is.ivanov@action-media.ru>
2022-09-04 10:39:58 +0300

    3.2. Зачем нужно приведение типов [#310019]

--- src/main/java/ru/job4j/oop/Plane.java
+++ src/main/java/ru/job4j/oop/Plane.java
@@ -1,6 +1,6 @@
 package ru.job4j.oop;
 
-public class Plane implements Vechicle{
+public class Plane implements Vechicle {
     @Override
     public void move() {
         System.out.println("Move through the air");
